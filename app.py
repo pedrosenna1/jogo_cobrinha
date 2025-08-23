2e37f5cc0df136fc397cb8eb118328d9aa3e92a5
Pedro Senna <pedro.senna@onbusdigital.com.br>
2025-08-23 11:22:59 -0300

    adicionando contato da cobrinha com o corpo, directions e velocidade

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import time
 import curses
+import random
 
 def game_loop(window):
     # Setup inicial
@@ -12,29 +13,83 @@ def game_loop(window):
         
     ]
 
-    current_direction = curses.KEY_RIGHT   
+    fruit = get_new_fruit(window=window)
+    current_direction = curses.KEY_RIGHT
+    timeout = 1000   
     
     while True:
         draw_screen(window=window)
-        draw_actor(actor = personagem,window=window)
-        direction = get_new_direction (window=window,timeout = 1000)
+        draw_snake(personagem=personagem,window=window)
+        draw_actor(actor=fruit,window=window,char=curses.ACS_DIAMOND)
+        direction = get_new_direction (window=window,timeout = timeout)
         if direction is None:
             direction = current_direction
-
-        move_actor(actor=personagem,direction=direction)
-        if actor_hit_border(actor=personagem,window = window):
+        if direction_is_opposite(direction=direction,current_direction = current_direction):
+            direction = current_direction
+    
+        move_snake(personagem=personagem,direction=direction)
+        if personagem[0] in personagem[1:]:
+            break
+        if snake_hit_border(personagem=personagem,window = window):
             return
+        if snake_hit_fruit(personagem=personagem,fruit=fruit):
+            snake_add(personagem=personagem)
+            if timeout > 200:
+                timeout = timeout - 100
+            elif timeout > 100:
+                timeout = timeout - 20
+            else:
+                timeout = timeout - 1
+            fruit = get_new_fruit(window=window)
+        
         current_direction = direction
+
+def direction_is_opposite(direction,current_direction):
+        match direction:
+            case curses.KEY_UP:
+                return current_direction == curses.KEY_DOWN
+            case curses.KEY_LEFT:
+                return current_direction == curses.KEY_RIGHT
+            case curses.KEY_DOWN:
+                return current_direction == curses.KEY_UP
+
+            case curses.KEY_RIGHT:
+                return current_direction == curses.KEY_LEFT
+
+
+def get_new_fruit(window):
+    height,width = window.getmaxyx()
+
+    return [random.randint(1,height-2),random.randint(1,width-2)]
+
+
+def snake_hit_border(personagem,window):
+    head = personagem[0]
+    return actor_hit_border(actor=head,window=window)
+
+def snake_hit_fruit(personagem,fruit):
+    return fruit in personagem
+
+def snake_add(personagem):
+    aumentar = personagem[-1][0] - 1
+    personagem.append([aumentar,15])
         
 def draw_screen(window):
     
     window.clear()
     window.border(0)
+
+
+def draw_snake(personagem, window):
+    head = personagem[0]
+    draw_actor(actor=head,window=window,char='@')
+    body = personagem[1:]
+    for body_part in body:
+        draw_actor(actor=body_part,window=window,char='s') 
     
-    
-def draw_actor(actor,window):
-    for numero in actor:
-        window.addch(numero[0],numero[1], curses.ACS_DIAMOND)
+def draw_actor(actor,window,char):
+    # for numero in actor:
+        window.addch(actor[0],actor[1], char)
 
 
 
@@ -47,28 +102,29 @@ def get_new_direction(window,timeout):
        return direction
     return None
 
+def move_snake(personagem,direction):
+    head = personagem[0].copy()
+    move_actor(actor=head,direction=direction)
+    personagem.insert(0,head)
+    personagem.pop()
+    
+        
+
 
 def move_actor(actor,direction):
         
         match direction:
             case curses.KEY_UP:
-                actor[3][0] -= 1
-                actor.append([actor[3][0],actor[3][1]])
-                actor.pop(0)
+                actor[0] -= 1
             case curses.KEY_LEFT:
-                actor[3][1] -= 1
-                actor.append([actor[3][0],actor[3][1]])
-                actor.pop(0)
+                actor[1] -= 1
             case curses.KEY_DOWN:
-                actor[3][0] += 1
-                actor.append([actor[3][0],actor[3][1]])
-                actor.pop(0)
+                actor[0] += 1
+
             case curses.KEY_RIGHT:
-                actor[3][1] += 1
-                actor.append([actor[3][0],actor[3][1]])
-                actor.pop(0)
-            case _:
-                pass
+                actor[1] += 1
+
+
 
 
 
@@ -76,9 +132,9 @@ def actor_hit_border(actor,window):
     height,width = window.getmaxyx()
         
 
-    if (actor[3][0] <=0) or (actor[3][0] >= height-1):
+    if (actor[0] <=0) or (actor[0] >= height-1):
         return True
-    if (actor[3][1] <=0) or (actor[3][1] >= width-1):
+    if (actor[1] <=0) or (actor[1] >= width-1):
         return True
     else:
         return False
